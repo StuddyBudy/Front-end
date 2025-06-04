7a258174515efc956f684399b229dadcde08bcce
Dhairya Shah <dhaishah7@gmail.com>
2025-06-04 17:20:31 -0400

    Migrated password generator to tailwind

---
+++ frontend/pass/pass.js
@@ -0,0 +1,67 @@
+function generatePassword(
+  length,
+  includeUppercase,
+  includeNumbers,
+  includeSymbols,
+) {
+  const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numberChars = "0123456789";
+  const symbolChars = "!@#$%^&*()_+[]{}|;:,.<>?";
+
+  let characterPool = lowercaseChars;
+
+  if (includeUppercase) {
+    characterPool += uppercaseChars;
+  }
+  if (includeNumbers) {
+    characterPool += numberChars;
+  }
+  if (includeSymbols) {
+    characterPool += symbolChars;
+  }
+
+  let password = "";
+  for (var i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * characterPool.length);
+    password += characterPool[randomIndex];
+  }
+
+  return password;
+}
+//function to check the length and dpending on the length change the words
+function lenCheck(length) {
+  const text = document.getElementById("text");
+  text.classList.remove("hidden");
+
+  if (length >= 13) {
+    text.innerHTML = "Great Password!";
+    text.style.backgroundColor = "#218838";
+  } else if (length < 13 && length >= 9) {
+    text.innerHTML = "Weak Password";
+    text.style.backgroundColor = "#F28048";
+  } else if (length <= 8) {
+    text.innerHTML = "Bad Password";
+    text.style.backgroundColor = "#750708";
+  }
+}
+//---------------------------------------------------------------------------------
+
+//calling functions
+document.getElementById("generate").addEventListener("click", () => {
+  const length = document.getElementById("length").value;
+  const includeUppercase = document.getElementById("includeUppercase").checked;
+  const includeNumbers = document.getElementById("includeNumbers").checked;
+  const includeSymbols = document.getElementById("includeSymbols").checked;
+
+  const generatedPassword = generatePassword(
+    length,
+    includeUppercase,
+    includeNumbers,
+    includeSymbols,
+  );
+  document.getElementById("password").innerText = generatedPassword;
+
+  lenCheck(length);
+  navigator.clipboard.writeText(document.getElementById("password").innerText);
+});
