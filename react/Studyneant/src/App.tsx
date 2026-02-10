bf814863c8963f99e25d61ae5acdd6000b042d22
XxCaptainNinjaxX <captainninjaflash@gmail.com>
2026-02-10 00:13:24 -0500

    created initial react app + started on Top Header

---
+++ react/Studyneant/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header from "./components/Top-Header/Header.tsx";
+
+function App() {
+    return (
+        <>
+            <Header />
+        </>
+    );
+}
+
+export default App;
